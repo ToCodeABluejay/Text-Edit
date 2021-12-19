a922d7a894c7e1f0567907cfc54a6ee1a60bdb62
Gabe Bauer <gabeb1277@gmail.com>
2021-12-19 14:57:37 -0500

    Fixed file-open

--- file.c
+++ file.c
@@ -28,7 +28,7 @@ void open(struct Window *w, struct File *f)
 	
 	if (f->fp)
 	{
-		fseek(f->fp, 0, SEEK_END);
+		fseek(f->fp, 0L, SEEK_END);
 		unsigned long long size = array_size(ftell(f->fp));
 		
 		w->contents = realloc(w->contents, size*sizeof(char));
